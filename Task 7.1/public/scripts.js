74b6355505db954be878a35639f74c40ab394715
Prince Patel <108411423+Prince1570@users.noreply.github.com>
2025-08-28 11:53:56 +1000

    Delete Task 7.1/public directory

--- Task 7.1/public/scripts.js
+++
@@ -1,8 +0,0 @@
-// Connect to the server socket
-const socket = io();
-
-// Listen to 'number' event
-socket.on('number', (msg) => {
-  console.log('Random number:', msg);
-  document.getElementById('number').innerText = msg;
-});
